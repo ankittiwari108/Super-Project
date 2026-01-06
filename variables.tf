2cb66ac7bca9d32f11a571db45a6569d2f8e6b41
Ankit tiwari <tiwari20398@gmail.com>
2026-01-07 01:20:24 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,16 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-east-2"
+  default = "us-east-1"
 }
 
 variable "aws_type" {
   description = "AWS ec2 type"
-  default = "t3.medium"
+  default = "c7i-flex.large"
 }
 
 variable "aws_ami" {
   description = "AWS ami"
-  default = "ami-022661f8a4a1b91cf"
+  default = "ami-0ecb62995f68bb549"
 }
+
+
